d658757e0a4bab3c2fc223fb3cbe135630e46a70
David Schwartz <dschwartz783@gmail.com>
2016-06-28 19:13:01 -0400

    add file writing, voice speaking event

--- app.js
+++ app.js
@@ -17,27 +17,44 @@ var client = new Discordie({autoReconnect: true});
 
 var auth = { token: "MTk3NDM2OTMwOTE5NDk3NzI4.ClRk-A.WArrWXpw9p8LmW7F91FpaZnRYfk" };
 var should_send = true;
+var recording_user = "";
+
 client.connect(auth);
 
 client.Dispatcher.on("MESSAGE_CREATE", (e) => {
     if (should_send) {
         const content = e.message.content;
         const channel = e.message.channel;
-        channel.sendMessage("Message Received");
+        if (content == "/record"){
+            channel.sendMessage("record command working");
+            e.message.member.getVoiceChannel().join();
+            recording_user = e.message.member;
+        }else if (content == "/stop"){
+            channel.sendMessage("recording stopped");
+            e.message.member.getVoiceChannel().leave();
+        }
         should_send = false;
     }else{
         should_send = true;
     }
 });
 
-client.Dispatcher.on("VOICE_CONNECTED", e => {
+client.Dispatcher.on("VOICE_SPEAKING", e => {
     e.voiceConnection.getDecoder()
-    .onPacket = (packet) =>
-{
-    const user = e.voiceConnection.ssrcToMember(packet.ssrc);
-    if (!user) return;
-    if (user.id != recordingUser) return;
     
-        var file = fs.appendFile("output.dat", packet)
+    .onPacket = (packet) =>
+    {
+        const user = e.voiceConnection.ssrcToMember(packet.ssrc);
+        if (!user) return;
+        if (user != recording_user) return;
+        recording_user.guild.generalChannel.sendMessage("voice received");
+        //this needs to be fixed
+        fs.appendFile('output.dat', packet, (err) => {
+  if (err) throw err;
+  console.log('The "data to append" was appended to file!');
+});
     }
 })
+/*client.Dispatcher.on("VOICE_CONNECTED", e => {
+    recording_user.guild.generalChannel.sendMessage("VOICE_CONNECTED");
+})*/
